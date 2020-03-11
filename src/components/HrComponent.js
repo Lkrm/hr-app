8544d3d907dd2a112cf12872a37f74af405e589b
biktor12487 <biktor12487@gmail.com>
2020-03-11 17:53:54 +0200

    fix route

--- src/components/HrComponent.js
+++ src/components/HrComponent.js
@@ -1,5 +1,23 @@
 import React from 'react';
+import { Header, LeftSideBar } from '../../../../containers';
+import { Layouts, NoInternetConnection } from '../../../../components';
 
-const HrComponent = () => <h1>Humen Resourses Module</h1>;
+const HrComponent = () => (
+  <Layouts.Main>
+    <LeftSideBar />
+    <div className="main-container">
+      <NoInternetConnection>
+        <Header hideToggleRightSideBar>
+          <h1 className="weight--medium title--xl title-page">
+            Human resources
+          </h1>
+        </Header>
+        <div className="main-container__content main-container__content--profile">
+          <h3> Profile container</h3>
+        </div>
+      </NoInternetConnection>
+    </div>
+  </Layouts.Main>
+);
 
 export default HrComponent;
